195f011fc61ad7c4579789971248927c12928318
Parnia_FEA <ParniaDabbagh238@gmail.com>
2025-02-01 16:17:16 +0330

    ScoreBoard is completed.

--- rogue.c
+++ rogue.c
@@ -11,6 +11,10 @@
 #define MAX_SIZE2 50
 #define LENGTH 214
 #define WIDTH 66
+#define COLOR_GOLD 10
+#define COLOR_SILVER 11
+#define COLOR_BRONZE 12
+
 
 int main() {
     setlocale(LC_ALL, "");
@@ -29,11 +33,12 @@ int main() {
     init_pair(7, COLOR_GREEN, COLOR_GREEN);
     init_pair(8, COLOR_RED, COLOR_RED);
     init_pair(9, COLOR_MAGENTA, COLOR_BLACK);
-    /*
-    printw("%s","\U0001f375");
-    refresh();
-    sleep(10);
-    */
+    init_color(COLOR_GOLD, 255, 215, 0);
+    init_color(COLOR_SILVER, 192, 192, 192);
+    init_color(COLOR_BRONZE, 205, 127, 50);
+    init_pair(10, COLOR_BLACK, COLOR_GOLD);
+    init_pair(11, COLOR_BLACK, COLOR_SILVER);
+    init_pair(12, COLOR_BLACK, COLOR_BRONZE);
     welcome_menu();
     endwin();
     return 0;
